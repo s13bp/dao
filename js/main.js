79dfb02c07853d6cb861a984c8a5b17b1c5d1400
s13bp <86206902+s13bp@users.noreply.github.com>
2022-05-01 15:45:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+const pages = document.querySelectorAll(".page");
+    const translateAmount = 100; 
+    let translate = 0;
+   
+
+    slide = (direction) => {
+
+      direction === "next" ? translate -= translateAmount : translate += translateAmount;
+
+      pages.forEach(
+        pages => (pages.style.transform = `translateX(${translate}%)`)
+       
+      );
+     
+    }
